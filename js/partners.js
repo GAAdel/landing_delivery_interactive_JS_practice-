56b199c853585b12a2a56efc4e5f77f80e28962e
GAAdel <32236445+GAAdel@users.noreply.github.com>
2021-11-08 22:56:46 +0300

    links

--- js/partners.js
+++ js/partners.js
@@ -6,7 +6,7 @@ const renderItems = (data) => {
       item;
     const a = document.createElement("a");
 
-    a.setAttribute("href", "/restaurant.html");
+    a.setAttribute("href", "restaurant.html");
     a.classList.add("card");
     a.classList.add("card-restaurant");
 
@@ -35,7 +35,7 @@ const renderItems = (data) => {
       e.preventDefault();
 
       localStorage.setItem("restaurant", JSON.stringify(item));
-      window.location.href = "/restaurant.html";
+      window.location.href = "restaurant.html";
     });
 
     cardsRestaurants.append(a);
